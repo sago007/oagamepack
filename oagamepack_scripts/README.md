297cd80368b3ec93a5b35a4ecac257d5dc77abfa
sago007 <poul@poulsander.com>
2017-11-19 14:36:52 +0100

    The oa.game/baseoa/entities.ent file is now generated from scripts.

---
+++ oagamepack_scripts/README.md
@@ -0,0 +1,3 @@
+The create_ent.py file is MIT licensed.
+
+The content of the csv-files are GPL2 or (at your option) any later version.
---
+++ oagamepack_scripts/create_output.bat
@@ -0,0 +1,2 @@
+rem This file is designed to be executed from a python3 version of the Anaconda terminal
+python create_ent.py > ../oa.game/baseoa/entities.ent
