c7ed83ac07afc1c587f5449860d854b081c13083
sago007 <poul@poulsander.com>
2018-09-25 19:30:19 +0200

    Updated script to also update oa.game

--- scripts/create_output.sh
+++ scripts/create_output.sh
@@ -1,2 +1,4 @@
 ./create_ent.py > ../install/OAPack/install/baseoa/entities.ent
 ./create_def.py > ../install/OAPack/install/baseoa/entities.def
+./create_ent.py > ../oa.game/baseoa/entities.ent
+./create_def.py > ../oa.game/baseoa/entities.def
