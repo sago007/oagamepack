671cad3b10726803f39d6acf4c5601fe585929e2
Neon_Knight <openarena.arg@gmail.com>
2018-11-11 01:40:48 -0300

    0.8.8 mapmodels and worldsounds, fixed obelisks for Hrv and Ovl, and changes to the compilation scripts.

--- scripts/create_output.sh
+++
@@ -1,4 +0,0 @@
-./create_ent.py > ../install/OAPack/install/baseoa/entities.ent
-./create_def.py > ../install/OAPack/install/baseoa/entities.def
-./create_ent.py > ../oa.game/baseoa/entities.ent
-./create_def.py > ../oa.game/baseoa/entities.def
